59e6e0379e4bc25823420d6f7a87923e005b66d5
Shaheen K <shaheenkader@outlook.com>
2020-05-31 09:06:55 +0530

    Update setup.py

--- python/setup.py
+++ python/setup.py
@@ -11,14 +11,10 @@ class build_py(_build_py):
         self.run_command("build_ext")
         return super().run()
 
-kaldi_root = os.getenv('KALDI_ROOT')
-kaldi_mkl = os.getenv('KALDI_MKL')
-source_path = os.getenv("VOSK_SOURCE", os.path.abspath(os.path.join(os.path.abspath(os.path.dirname(__file__)), "../src")))
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-if kaldi_root == None:
-    print("Define KALDI_ROOT")
-    exit(1)
-
+source_path=os.getenv("VOSK_SOURCE", os.path.abspath(os.path.join(os.path.abspath(os.path.dirname(__file__)), "../src")))
 distutils.log.set_verbosity(distutils.log.DEBUG)
 distutils.dir_util.copy_tree(
     source_path,
@@ -26,9 +22,7 @@ distutils.dir_util.copy_tree(
     update=1,
     verbose=1)
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
+kaldi_root = 'opt/kaldi'
 kaldi_static_libs = ['src/online2/kaldi-online2.a',
              'src/decoder/kaldi-decoder.a',
              'src/ivector/kaldi-ivector.a',
@@ -47,26 +41,21 @@ kaldi_static_libs = ['src/online2/kaldi-online2.a',
              'src/base/kaldi-base.a',
              'tools/openfst/lib/libfst.a',
              'tools/openfst/lib/libfstngram.a']
-kaldi_link_args = ['-s']
+kaldi_link_args = ['-s', '-lmkl_rt']
 kaldi_libraries = []
 
-if sys.platform.startswith('darwin'):
-    kaldi_link_args.extend(['-Wl,-undefined,dynamic_lookup', '-framework', 'Accelerate'])
-elif kaldi_mkl != None:
-    kaldi_link_args.extend(['-L/opt/intel/mkl/lib/intel64', '-Wl,-rpath=/opt/intel/mkl/lib/intel64'])
-    kaldi_libraries.extend(['mkl_rt', 'mkl_intel_lp64', 'mkl_core', 'mkl_sequential'])
-else:
-    kaldi_static_libs.append('tools/OpenBLAS/libopenblas.a')
-    kaldi_libraries.append('gfortran')
+lx = [kaldi_root + '/' + x for x in kaldi_static_libs]
+
+lx.append('/opt/intel/mkl/lib/intel64/libmkl_rt.so')
 
 sources = ['kaldi_recognizer.cc', 'model.cc', 'spk_model.cc', 'vosk_api.cc', 'vosk.i']
 
 vosk_ext = Extension('vosk._vosk',
                     define_macros = [('FST_NO_DYNAMIC_LINKING', '1')],
-                    include_dirs = [kaldi_root + '/src', kaldi_root + '/tools/openfst/include', 'vosk'],
+                    include_dirs = [kaldi_root + '/src', kaldi_root + '/tools/openfst/include', 'vosk', '/opt/intel/mkl/include'],
                     swig_opts=['-outdir', 'vosk', '-c++'],
                     libraries = kaldi_libraries,
-                    extra_objects = [kaldi_root + '/' + x for x in kaldi_static_libs],
+                    extra_objects = lx,
                     sources = ['vosk/' + x for x in sources],
                     extra_link_args = kaldi_link_args,
                     extra_compile_args = ['-std=c++11', '-Wno-sign-compare', '-Wno-unused-variable', '-Wno-unused-local-typedefs'])
